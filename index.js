fc0c0ed63d4c5cec2d60b99eaac27618fae2f07e
gonthegeek <gronzon31@gmail.com>
2022-03-06 22:15:06 -0600

    v2

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ var fechaHora = new Date();
 var fechaFiesta = new Date("04/02/2022");
 
 var diferencia = fechaFiesta.getTime() - fechaHora.getTime();
-var dias = Math.round(diferencia/(1000*60*60*24));
+var dias = Math.round(diferencia / (1000 * 60 * 60 * 24));
 
+document.getElementById("reloj").innerHTML = dias + " dias ";
 
+$(function() {
 
-document.getElementById("reloj").innerHTML =
- dias+" dias "
+    $('[data-toggle="modal"]').hover(function() {
+      var modalId = $(this).data('target');
+      $(modalId).modal('show');
+  
+    });
+  
+  });
